5792fba91bcc1ad50c2ec05121ba602303779693
vabianchacha <fabianchacha03gmail.com>
2025-02-21 10:44:40 +0300

    6th commit

---
+++ medapp/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from medapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('stater/', views.stater, name='stater'),
+
+
+]
