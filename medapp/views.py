da9a85cf9c92fce742fe58d944c506de4ba889be
KC-coddy <mariocastro1738@GMAIL.COMgit config --global user.email mariocastro1738@gmail.com>
2025-03-11 08:07:34 +0300

    first commit

--- medapp/views.py
+++ medapp/views.py
@@ -1,3 +1,6 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
 from medapp.models import *
 
@@ -63,8 +66,45 @@ def edit(request,id):
         return render(request, 'edit.html', {'appointment': appointment})
 
 def register(request):
-    return render(request,'register.html')
+    """ Show the registration form """
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        # Check the password
+        if password == confirm_password:
+            try:
+                user = User.objects.create_user(username=username, password=password)
+                user.save()
+
+                # Display a message
+                messages.success(request, "Account created successfully")
+                return redirect('/login')
+            except:
+                # Display a message if the above fails
+                messages.error(request, "Username already exist")
+        else:
+            # Display a message saying passwords don't match
+            messages.error(request, "Passwords do not match")
+
+    return render(request, 'register.html')
 
 
 def login_view (request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        # Check if the user exists
+        if user is not None:
+            # login(request, user)
+            login(request, user)
+            messages.success(request, "You are now logged in!")
+            return redirect('/home')
+        else:
+            messages.error(request, "Invalid login credentials")
+
     return render(request, 'login.html')
